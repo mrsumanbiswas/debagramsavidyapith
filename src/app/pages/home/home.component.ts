ac832cfad0c9eab444cf3ae54a645b45c394a639
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-06 15:30:53 +0530

    slider img

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  slides = [
+    { img: '../assets/background.jpg' },
+    { img: '../assets/background.jpg' },
+    { img: '../assets/background.jpg' },
+    { img: '../assets/background.jpg' },
+    { img: '../assets/background.jpg' },
+    { img: '../assets/background.jpg' },
+  ];
+
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay:true,
+
+
+  };
 
   constructor() { }
 
