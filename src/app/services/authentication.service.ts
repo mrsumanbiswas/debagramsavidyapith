cb1c553b00354e9c7c1251a1f1590966e555d0e7
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-15 01:59:17 +0530

    some chenges

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -3,10 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthenticationService {
 
-
-
-  constructor(
-  ) { }
+  constructor() { }
 }
--- src/app/services/database.service.spec.ts
+++ src/app/services/database.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import { DatabaseService } from './database.service';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('DatabaseService', () => {
+  let service: DatabaseService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    service = TestBed.inject(DatabaseService);
   });
 
   it('should be created', () => {
