1c42a4ceebb23f66987473c7505ee1824b7b2657
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-19 09:10:31 +0530

    authentication with google done

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Auth, GoogleAuthProvider, signOut, signInWithPopup, authState } from '@angular/fire/auth';
+import { FirestoreDatabaseService } from './firestore-database.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  isLogedIn(): boolean {
+    // saving to session storage
+    this.auth.onAuthStateChanged(
+      (user) => {
+        sessionStorage.setItem(
+          'isLogedin', JSON.stringify(
+            (user?.uid === localStorage.getItem('uid'))
+          )
+        )
+      }
+    )
+    // retriving from seeion storage
+    return (sessionStorage.getItem('isLogedin')) ? true : false
+  }
+
+  signIn() {
+    const credential = signInWithPopup(this.auth, new GoogleAuthProvider()).then(
+      (credential) => {
+        sessionStorage.setItem('user', JSON.stringify(credential.user))
+      }
+    ).finally(
+      () => {
+        const data = sessionStorage.getItem('user')
+        if (data !== null) {
+          var user = JSON.parse(data)
+          // saving data to local storage
+          localStorage.setItem('uid', user.uid)
+          localStorage.setItem('photoURL', user.photoURL)
+          localStorage.setItem('displayName', user.displayName)
+          // saving data to detabase
+          this.database.setData('user', user.uid, user)
+        }
+      }
+    )
+  }
+
+
+  // logout/signout
+  async signOut() {
+    await signOut(this.auth);
+    localStorage.clear();
+    sessionStorage.clear();
+  }
+
+  constructor(private auth: Auth, private database: FirestoreDatabaseService) { }
+
 }
