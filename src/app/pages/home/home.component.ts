d4d29d0e357aadd1cb69077ed63a0402d0336035
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-23 23:22:44 +0530

    master prototype

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -29,4 +29,33 @@ export class HomeComponent {
     )
   }
 
+
+  displayedColumns: string[] = [
+    'class-v',
+    'class-vi',
+    'class-vii-viii',
+    'class-ix-x',
+    'class-xi-xii'
+  ];
+  subjectData = ELEMENT_DATA;
 }
+
+export interface PeriodicElement {
+  name: string;
+  weight: number;
+  symbol: string;
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { name: '', weight: 1.0079, symbol: 'H' },
+  { name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { name: 'Boron', weight: 10.811, symbol: 'B' },
+  { name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
