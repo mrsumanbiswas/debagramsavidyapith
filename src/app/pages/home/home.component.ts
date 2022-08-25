2b8bf6911f49e6273c4e354656ac8e4fce63f4aa
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-26 00:30:45 +0530

    home page TS complete

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { homePage } from 'src/app/models/home-page-data';
 import { FirestoreDatabaseService } from 'src/app/services/firestore-database.service';
 
@@ -7,10 +8,20 @@ import { FirestoreDatabaseService } from 'src/app/services/firestore-database.se
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   data!: homePage | any;
 
-  slideConfig = {
+  // displayed columns
+  displayedColumns: string[] = [
+    'i',
+    'ii',
+    'iii',
+    'iv',
+    'v'
+  ];
+
+  // slider config
+  slider_image = {
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
@@ -19,8 +30,30 @@ export class HomeComponent {
     autoplaySpeed: 3000
   };
 
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1500
+  };
+
+  // form group and it's validations
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.email],
+  });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+
   constructor(
-    firestore_database: FirestoreDatabaseService) {
+    firestore_database: FirestoreDatabaseService,
+    private _formBuilder: FormBuilder
+  ) {
     // home page data pulling from google firestore datbase
     firestore_database.getData('page', 'home').then(
       (value) => {
@@ -29,33 +62,17 @@ export class HomeComponent {
     )
   }
 
-
-  displayedColumns: string[] = [
-    'class-v',
-    'class-vi',
-    'class-vii-viii',
-    'class-ix-x',
-    'class-xi-xii'
-  ];
-  subjectData = ELEMENT_DATA;
+  ngOnInit(): void {
+    // slide config
+    let x = 1, y = 1;
+    if (innerWidth >= 1024) {
+      x = 4;
+      y = 2;
+    } else if (innerWidth < 1024 && innerWidth > 425) {
+      x = 2;
+      y = 1;
+    }
+    this.slideConfig.slidesToShow = x;
+    this.slideConfig.slidesToScroll = y;
+  }
 }
-
-export interface PeriodicElement {
-  name: string;
-  weight: number;
-  symbol: string;
-}
-
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { name: '', weight: 1.0079, symbol: 'H' },
-  { name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { name: 'Boron', weight: 10.811, symbol: 'B' },
-  { name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
