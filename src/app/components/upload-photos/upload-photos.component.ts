bd09a4d099f7ff9a5b380e155b403b0b9e090ea9
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-28 16:47:37 +0530

    upload photos

--- src/app/components/upload-photos/upload-photos.component.ts
+++ src/app/components/upload-photos/upload-photos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RealtimeDatabaseService } from 'src/app/services/realtime-database.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-upload-photos',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-photos.component.scss']
 })
 export class UploadPhotosComponent implements OnInit {
+  uploadfraction!: number;
+  fileData!: File;
 
-  constructor() { }
+  constructor(
+    private storage: StorageService,
+    private database: RealtimeDatabaseService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  photoSelected(files: FileList | null) {
+    const file = files?.item(0)
+    if (file !== null && file !== undefined) {
+      this.fileData = file
+    }
+
+  }
+
+  // uploads photo to storage and creates a data to the realtime database
+  uploadPhoto() {
+    if (
+      sessionStorage.getItem('isLogedIn') === 'false'
+    ) {
+      this.storage.uploadFile('photos', Date.now().toString(), this.fileData)
+        .then(
+          (snapshot) => {
+            this.database.writeData('photos', snapshot.metadata.name, {
+              vote: 0,
+              name: snapshot.metadata.name,
+              uid: localStorage.getItem('uid'),
+              url: sessionStorage.getItem('photo_url_' + snapshot.metadata.name),
+              timestamp: snapshot.metadata.timeCreated
+            })
+          },
+          (err) => { console.log(err); }
+        )
+    } else {
+      alert(
+        'Please sign-in to upload a photo'
+      );
+
+    }
   }
 
 }
