d938586febc8539d3264efbf6b34d2505379f16e
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-28 16:47:09 +0530

    some fixes

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
 import { getDownloadURL, ref, Storage, uploadBytesResumable, deleteObject } from '@angular/fire/storage';
-import { fileData } from '../models/file-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  uploadFile(base_path: string, fileData: fileData) {
+  uploadFile(base_path: string, id: string, fileData: File) {
+    const Ref = ref(this.storage, `${base_path}/${id}`);
     // Upload file 
     const uploadTask = uploadBytesResumable(
-      ref(this.storage, `${base_path}/${fileData.name}`),
-      fileData.file
-    )
+      Ref,
+      fileData, { contentType: fileData.type, customMetadata: { name: fileData.name, size: fileData.size.toString(), lastModified: fileData.lastModified.toString() } }
+    );
 
     // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on('state_changed',
+    uploadTask.on(
+      'state_changed',
       (snapshot) => {
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        sessionStorage.setItem('photo_progress', progress.toString())
         console.log('Upload is ' + progress + '% done');
+        // tells about the state of the file upload
         switch (snapshot.state) {
           case 'paused':
             console.log('Upload is paused');
@@ -33,36 +36,37 @@ export class StorageService {
         switch (error.code) {
           case 'storage/unauthorized':
             // User doesn't have permission to access the object
+            console.log('storage/unauthorized');
             break;
           case 'storage/canceled':
             // User canceled the upload
+            console.log('storage/canceled');
             break;
 
           // ...
-
           case 'storage/unknown':
             // Unknown error occurred, inspect error.serverResponse
+            console.log('storage/unknown');
             break;
         }
       },
       () => {
         // Upload completed successfully, now we can get the download URL
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log('File available at', downloadURL);
-        })
+          sessionStorage.setItem('photo_url_' + uploadTask.snapshot.metadata.name, downloadURL)
+        });
       }
     )
 
-    // Returning uploadTusk for further programs
-    return uploadTask
+    return uploadTask;
   }
 
-  deleteFile(base_path: string, fileData: fileData) {
+  deleteFile(base_path: string, id: string) {
     // Delete the file
     deleteObject(
       ref(
         this.storage,
-        `${base_path}/${fileData.name}`
+        `${base_path}/${id}`
       )
     ).then(() => {
       // File deleted successfully
