d6ebd7dc29c41465d261dc3333e8840661b87bb5
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-29 12:09:40 +0530

    databse automated

--- src/app/components/upload-photos/upload-photos.component.ts
+++ src/app/components/upload-photos/upload-photos.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { RealtimeDatabaseService } from 'src/app/services/realtime-database.service';
 import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
@@ -13,7 +12,6 @@ export class UploadPhotosComponent implements OnInit {
 
   constructor(
     private storage: StorageService,
-    private database: RealtimeDatabaseService
   ) { }
 
   ngOnInit(): void { }
@@ -31,19 +29,13 @@ export class UploadPhotosComponent implements OnInit {
     if (
       sessionStorage.getItem('isLogedIn') === 'false'
     ) {
-      this.storage.uploadFile('photos', Date.now().toString(), this.fileData)
-        .then(
+      this.storage.uploadFile('photos', Date.now().toString(), this.fileData, 'realtime')
+        .on('state_changed',
           (snapshot) => {
-            this.database.writeData('photos', snapshot.metadata.name, {
-              vote: 0,
-              name: snapshot.metadata.name,
-              uid: localStorage.getItem('uid'),
-              url: sessionStorage.getItem('photo_url_' + snapshot.metadata.name),
-              timestamp: snapshot.metadata.timeCreated
-            })
-          },
-          (err) => { console.log(err); }
+            this.uploadfraction = (snapshot.bytesTransferred / snapshot.totalBytes);
+          }
         )
+
     } else {
       alert(
         'Please sign-in to upload a photo'
